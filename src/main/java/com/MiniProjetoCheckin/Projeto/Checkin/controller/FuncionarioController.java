966ea5bf8f3179c5888d24d7801f140e02df0872
Camila <camiart@gmail.com>
2023-11-21 18:25:37 -0300

    exercicio 09

--- src/main/java/com/MiniProjetoCheckin/Projeto/Checkin/controller/FuncionarioController.java
+++ src/main/java/com/MiniProjetoCheckin/Projeto/Checkin/controller/FuncionarioController.java
@@ -5,55 +5,79 @@ import com.MiniProjetoCheckin.Projeto.Checkin.dto.FuncionarioDTO;
 import com.MiniProjetoCheckin.Projeto.Checkin.dto.PontoDTO;
 import com.MiniProjetoCheckin.Projeto.Checkin.model.Funcionario;
 import com.MiniProjetoCheckin.Projeto.Checkin.service.FuncionarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 
 
 @RestController
 @RequestMapping("/funcionarios")
+@Validated
 public class FuncionarioController {
 
     private final FuncionarioService funcionarioService;
+    private static final Logger logger = LoggerFactory.getLogger(FuncionarioController.class);
 
     @Autowired
     public FuncionarioController(FuncionarioService funcionarioService) {
         this.funcionarioService = funcionarioService;
     }
 
-    @PostMapping
-    public ResponseEntity<Funcionario> criarFuncionario(@Valid @RequestBody FuncionarioDTO funcionarioDTO) {
-        Funcionario novoFuncionario = funcionarioService.criarFuncionario(funcionarioDTO);
-        return new ResponseEntity<>(novoFuncionario, HttpStatus.CREATED);
-    }
-
+    @Operation(summary = "Listar funcionários paginados")
     @GetMapping
     public ResponseEntity<Page<FuncionarioDTO>> listarFuncionarios(
+            @Parameter(description = "Número da página (padrão = 0)")
             @RequestParam(defaultValue = "0") int pagina,
+            @Parameter(description = "Tamanho da página (padrão = 12)")
             @RequestParam(defaultValue = "12") int tamanhoPagina) {
-
         Page<FuncionarioDTO> funcionariosPaginados = funcionarioService.listarFuncionariosPaginados(pagina, tamanhoPagina);
         return ResponseEntity.ok(funcionariosPaginados);
     }
 
+    @Operation(summary = "Adicionar ponto para um funcionário")
     @PostMapping("/{id}/registros")
     public ResponseEntity<?> adicionarPontoParaFuncionario(
+            @Parameter(description = "ID do Funcionário", required = true)
             @PathVariable Long id,
+            @Parameter(description = "Objeto do Ponto a ser adicionado", required = true)
             @RequestBody PontoDTO pontoDTO) {
         ResponseEntity<FuncionarioDTO> response = funcionarioService.adicionarPontoParaFuncionario(id, pontoDTO);
+        if (response.getStatusCode().isError()) {
+            logger.error("Erro ao adicionar ponto para o funcionário com ID {}: {}", id, response.getStatusCodeValue());
+        }
         return ResponseEntity.status(response.getStatusCode()).build();
     }
 
+    @Operation(summary = "Buscar um funcionário por ID")
     @GetMapping("/{id}")
-    public ResponseEntity<FuncionarioDTO> buscarFuncionarioPorId(@PathVariable Long id) {
+    public ResponseEntity<FuncionarioDTO> buscarFuncionarioPorId(
+            @Parameter(description = "ID do Funcionário", required = true)
+            @PathVariable Long id) {
         FuncionarioDTO funcionario = funcionarioService.buscarDetalhesFuncionario(id);
         if (funcionario != null) {
             return ResponseEntity.ok(funcionario);
         } else {
+            logger.error("Funcionário não encontrado com o ID: {}", id);
             return ResponseEntity.notFound().build();
         }
+    }
+
+
+    @Operation(summary = "Criar um novo funcionário")
+    @PostMapping
+    public ResponseEntity<Funcionario> criarFuncionario(
+            @Valid @RequestBody FuncionarioDTO funcionarioDTO) {
+        logger.info("Criando um novo funcionário: {}", funcionarioDTO);
+        Funcionario novoFuncionario = funcionarioService.criarFuncionario(funcionarioDTO);
+        return new ResponseEntity<>(novoFuncionario, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/MiniProjetoCheckin/Projeto/Checkin/controller/RegistroPontoController.java
+++ src/main/java/com/MiniProjetoCheckin/Projeto/Checkin/controller/RegistroPontoController.java
@@ -2,6 +2,8 @@ package com.MiniProjetoCheckin.Projeto.Checkin.controller;
 
 import com.MiniProjetoCheckin.Projeto.Checkin.model.TipoRegistro;
 import com.MiniProjetoCheckin.Projeto.Checkin.service.FuncionarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +19,13 @@ public class RegistroPontoController {
         this.funcionarioService = funcionarioService;
     }
 
+    @Operation(summary = "Registrar ponto para um funcionário")
     @PostMapping("/{idFuncionario}")
     public ResponseEntity<?> registrarPonto(
+            @Parameter(description = "ID do Funcionário", required = true)
             @PathVariable Long idFuncionario,
+            @Parameter(description = "Tipo do Registro", required = true)
             @RequestBody TipoRegistro tipoRegistro) {
-
         ResponseEntity<?> response = funcionarioService.registrarPonto(idFuncionario, tipoRegistro);
         return ResponseEntity.status(response.getStatusCode()).build();
     }
